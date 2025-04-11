c07071210ca0115f3541c3eb683c012e08795de0
Jude Markabawi <34716142+judemarkabawi@users.noreply.github.com>
2025-04-11 00:34:21 -0700

    driver_macros: device_impls! now allows multiple devices sharing an implementation

--- crates/driver_macros/src/lib.rs
+++ crates/driver_macros/src/lib.rs
@@ -4,65 +4,84 @@ use convert_case::{Case, Casing};
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
-use syn::{braced, bracketed, parse::Parse, parse_macro_input, Ident, LitInt, Token};
+use syn::{
+    braced, bracketed, parse::Parse, parse_macro_input, punctuated::Punctuated, Ident, LitInt,
+    Token,
+};
 
-struct DeviceDefs(Vec<DeviceDef>);
+struct DeviceDefs(Vec<SharedDeviceDef>);
 
 impl Parse for DeviceDefs {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let content;
         bracketed!(content in input);
 
-        let mut device_defs = Vec::new();
-        while !content.is_empty() {
-            let device_impl: DeviceDef = content.parse()?;
-            device_defs.push(device_impl);
-
-            content.parse::<Token![,]>()?;
-        }
-
+        let device_defs = Punctuated::<SharedDeviceDef, Token![,]>::parse_terminated(&content)?
+            .into_iter()
+            .collect();
         Ok(DeviceDefs(device_defs))
     }
 }
-struct DeviceDef {
-    name: Ident,
-    product_id: u16,
+
+/// Definitions for one or more devices, along with their shared function mappings.
+struct SharedDeviceDef {
+    device_ids: Vec<DeviceId>,
     functions: Vec<FunctionMapping>,
 }
 
-struct FunctionMapping {
-    feature: Ident,
-    impl_fn: Ident,
-}
-
-impl Parse for DeviceDef {
+impl Parse for SharedDeviceDef {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let name = input.parse::<Ident>()?;
-        let product_id: u16 = input.parse::<LitInt>()?.base10_parse()?;
+        // Parse one or more `name product_id` listed
+        let device_ids = Punctuated::<DeviceId, Token![|]>::parse_separated_nonempty(input)?
+            .into_iter()
+            .collect();
 
+        // Then the rest is in braces
         let content;
         braced!(content in input);
 
-        let mut functions = Vec::new();
-        while !content.is_empty() {
-            let feature = content.parse()?;
-            content.parse::<Token![:]>()?;
-            let impl_fn = content.parse()?;
-            functions.push(FunctionMapping { feature, impl_fn });
-
-            // Even last line should end in a comma for better git diffs
-            content.parse::<Token![,]>()?;
-        }
-
-        Ok(DeviceDef {
-            name,
-            product_id,
+        // Zero or more feature: impl_fn mappings in the rbaces
+        let functions = Punctuated::<FunctionMapping, Token![,]>::parse_terminated(&content)?
+            .into_iter()
+            .collect();
+        Ok(SharedDeviceDef {
+            device_ids,
             functions,
         })
     }
 }
 
-impl DeviceDef {
+impl SharedDeviceDef {
+    /// This flattens a `SharedDeviceDef` into a list of `SingleDeviceDef`s that can be iterated over easier
+    /// as if they were each just one device and its implementation.
+    fn flatten_devices(&self) -> impl Iterator<Item = SingleDeviceDef<'_>> {
+        self.device_ids.iter().map(|device_id| SingleDeviceDef {
+            device_id,
+            functions: &self.functions,
+        })
+    }
+}
+
+struct SingleDeviceDef<'a> {
+    device_id: &'a DeviceId,
+    functions: &'a [FunctionMapping],
+}
+
+/// Device name (ex: `DeathadderV2ProWireless`) and USB product id
+struct DeviceId {
+    name: Ident,
+    product_id: u16,
+}
+
+impl Parse for DeviceId {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let name = input.parse::<Ident>()?;
+        let product_id: u16 = input.parse::<LitInt>()?.base10_parse()?;
+        Ok(DeviceId { name, product_id })
+    }
+}
+
+impl DeviceId {
     fn pascal_name(&self) -> Ident {
         Ident::new(
             &self.name.to_string().to_case(Case::Pascal),
@@ -78,6 +97,43 @@ impl DeviceDef {
     }
 }
 
+/// A mapping of a trait method in `FeatureSet` to a concrete implementation
+struct FunctionMapping {
+    feature: Ident,
+    impl_fn: Ident,
+}
+
+impl Parse for FunctionMapping {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let feature = input.parse()?;
+        input.parse::<Token![:]>()?;
+        let impl_fn = input.parse()?;
+        Ok(FunctionMapping { feature, impl_fn })
+    }
+}
+
+/// For each device, this macro:
+///     - Defines a custom struct for it
+///     - Defines the `FeatureSet` trait on it, only implementing listed methods using their described impls
+///     - Defines the product_id of that device
+///     - Adds a match arm to the `get_device_impl`, which maps from its product_id to its custom struct
+/// We then end up with implementations of subsets of `FeatureSet`'s methods for
+/// each device, as well as a method `get_device_impl` to take a product_id and return a `Box<dyn FeatureSet>` or error.
+///
+/// Example use:
+/// ```
+/// device_impls!([
+///     DeathadderV2ProWired    0x007C |
+///     DeathadderV2ProWireless 0x007D
+///     {
+///         get_dpi: get_dpi_0x3f,
+///         set_dpi: set_dpi_0x3f,
+///     },
+///     ViperMini 0x008A {
+///         get_dpi_stages: etc,
+///     }
+/// ]);
+/// ```
 #[proc_macro]
 pub fn device_impls(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeviceDefs);
@@ -85,17 +141,25 @@ pub fn device_impls(input: TokenStream) -> TokenStream {
 }
 
 fn device_impls_inner(device_defs: &DeviceDefs) -> TokenStream2 {
-    let DeviceDefs(device_defs) = device_defs;
+    let device_defs: Vec<SingleDeviceDef> = device_defs
+        .0
+        .iter()
+        .flat_map(|shared_def| shared_def.flatten_devices())
+        .collect();
+
+    let device_ids = device_defs.iter().map(|device_def| device_def.device_id);
 
     // Make sure all product IDs are unique. Duplicates are a guaranteed programmer bug
-    if let Some(error) = find_first_duplicate(device_defs) {
+    if let Some(error) = find_first_duplicate(device_ids) {
         return error.into_compile_error();
     }
 
-    let caps_names = device_defs.iter().map(|device_def| device_def.caps_name());
+    let caps_names = device_defs
+        .iter()
+        .map(|device_def| device_def.device_id.caps_name());
     let pascal_names = device_defs
         .iter()
-        .map(|device_def| device_def.pascal_name());
+        .map(|device_def| device_def.device_id.pascal_name());
     let device_impls = device_defs.iter().map(device_impl_inner);
 
     quote! {
@@ -114,14 +178,14 @@ fn device_impls_inner(device_defs: &DeviceDefs) -> TokenStream2 {
     }
 }
 
-fn device_impl_inner(device_def: &DeviceDef) -> TokenStream2 {
-    let DeviceDef {
-        name: _,
-        product_id,
+fn device_impl_inner(device_def: &SingleDeviceDef<'_>) -> TokenStream2 {
+    let SingleDeviceDef {
+        device_id,
         functions,
     } = device_def;
-    let caps_name = device_def.caps_name();
-    let pascal_name = device_def.pascal_name();
+    let caps_name = device_id.caps_name();
+    let pascal_name = device_id.pascal_name();
+    let product_id = device_id.product_id;
 
     let fn_impls: syn::Result<Vec<TokenStream2>> = functions.iter().map(|fn_map| {
             let FunctionMapping { feature, impl_fn } = fn_map;
@@ -195,20 +259,21 @@ fn device_impl_inner(device_def: &DeviceDef) -> TokenStream2 {
     }
 }
 
-fn find_first_duplicate<'a, T>(device_defs: T) -> Option<syn::Error>
+/// Find duplicate product IDs in a list of device definitions for debugging.
+fn find_first_duplicate<'a, T>(device_ids: T) -> Option<syn::Error>
 where
-    T: IntoIterator<Item = &'a DeviceDef>,
+    T: IntoIterator<Item = &'a DeviceId>,
 {
     let mut set = HashSet::new();
-    for device_def in device_defs {
-        let product_id = device_def.product_id;
+    for device_id in device_ids {
+        let product_id = device_id.product_id;
 
         if set.contains(&product_id) {
             return Some(syn::Error::new(
-                device_def.name.span(),
+                device_id.name.span(),
                 format!(
                     "{:#06x} is a duplicate USB product id.",
-                    device_def.product_id
+                    device_id.product_id
                 ),
             ));
         }
